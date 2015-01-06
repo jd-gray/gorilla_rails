8e13ca9cc5eb2182f801a697b91434329843097c
Jared Gray <jd.gray@yahoo.com>
2015-01-05 18:42:08 -0800

    completed inital part of posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,50 @@
 class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update]
+
+	def index
+		@posts = Post.all.order("created_at DESC")
+	end
+
+	def show
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+
+		if @post.save
+			redirect_to @board, notice: "Created new post!"
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @post.update(post_params)
+			redirect_to @board, notice: "Post successfully updated!"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :description)
+	end
+
+	def find_post
+		@post = Post.find(params[:id])
+	end
 end

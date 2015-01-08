28e83ca814d514810cb95b8d575933f22f03f4c0
Jared Gray <jd.gray@yahoo.com>
2015-01-08 15:57:10 -0800

    created welcome page, minimal styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do 
 
+	root 'welcome#index'
+
 	resources :users, only: [:index, :create, :show]
   	get '/signup' => 'users#new'
   	get '/login'     => 'sessions#new'
 	post '/login'    => 'sessions#create'
 	delete '/logout' => 'sessions#destroy'
 
-	resources :posts
-
-  	root 'posts#index' 
+	resources :posts 
  
 end

1e9fdc010504ea6b9b9ac64461f2fff3be69d436
Jared Gray <jd.gray@yahoo.com>
2015-01-08 18:55:32 -0800

    added timestamps for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,14 @@
 class Post
   include Mongoid::Document
+  include Mongoid::Timestamps
   belongs_to :user
 
   field :title, type: String
   field :description, type: String
 
   mount_uploader :image, AvatarUploader
+
+  def date_published
+  	created_at.localtime.strftime("%A, %B %-d, %Y at %l:%M %p")
+  end
 end
